a3b56ce10a99b0ad5473721ece5c67ea4f6b8e08
Bhushan Junagade <137473467+bhushanio@users.noreply.github.com>
2023-08-28 13:06:42 +0530

    LambdaFunction.py

--- LambdaFunction.py
+++ LambdaFunction.py
@@ -2,10 +2,10 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 
-#Provide security group id in which the intruder will be moved after performing any malicious activity
+#Provide Security Group id in Which the Intruder Will be Moved After Performing any Malicious Activity
 isolated_sg = '<instance_id_here>'
 
-#Function to change Group of Intruder
+#Function to Change Group of Intruder
 def lambda_handler(event, context):
 
   #Using "event" tag to gather info of "FINDINGS" & "AFFECTED INSTANCE"
@@ -16,7 +16,7 @@ def lambda_handler(event, context):
   print(f"Finding type: {finding_type}")
   print(f"Instance ID: {instance_id}")
 
-  #Matching finding type with "FINDINGS"
+  #Matching Finding Type Using "FINDINGS"
   if finding_type == 'Recon:EC2/Portscan':
     victim_ec2 = ec2.Instance(instance_id)
     victim_ec2.modify_attribute(Groups=[isolated_sg])
